0899222a273fad1d3d58c1a96fdfc60996ab8970
往日。 <mwr123607@126.com>
2016-09-04 12:17:00 +0800

    init

---
+++ MemoryAssistant/MemorizePeopleForm.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace MemoryAssistant
+{
+    partial class MemorizePeopleForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lab_ShowInfo = new System.Windows.Forms.Label();
+            this.btn_Know = new System.Windows.Forms.Button();
+            this.btn_Unkown = new System.Windows.Forms.Button();
+            this.chkBox_ModeSelect = new System.Windows.Forms.CheckBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.pBox_ShowPhotos = new System.Windows.Forms.PictureBox();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.工具ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.重置熟悉度ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.帮助ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.关于ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.progressBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.Lab_ShowRem = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.更新日志ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox_ShowPhotos)).BeginInit();
+            this.menuStrip.SuspendLayout();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lab_ShowInfo
+            // 
+            this.lab_ShowInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lab_ShowInfo.Font = new System.Drawing.Font("微软雅黑", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lab_ShowInfo.Location = new System.Drawing.Point(3, 0);
+            this.lab_ShowInfo.Name = "lab_ShowInfo";
+            this.lab_ShowInfo.Size = new System.Drawing.Size(264, 345);
+            this.lab_ShowInfo.TabIndex = 1;
+            this.lab_ShowInfo.Text = "点击查看信息";
+            this.lab_ShowInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lab_ShowInfo.Click += new System.EventHandler(this.lab_ShowInfo_Click);
+            // 
+            // btn_Know
+            // 
+            this.btn_Know.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_Know.Location = new System.Drawing.Point(3, 3);
+            this.btn_Know.Name = "btn_Know";
+            this.btn_Know.Size = new System.Drawing.Size(126, 137);
+            this.btn_Know.TabIndex = 2;
+            this.btn_Know.Text = "认识";
+            this.btn_Know.UseVisualStyleBackColor = true;
+            this.btn_Know.Click += new System.EventHandler(this.btn_Know_Click);
+            // 
+            // btn_Unkown
+            // 
+            this.btn_Unkown.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_Unkown.Location = new System.Drawing.Point(135, 3);
+            this.btn_Unkown.Name = "btn_Unkown";
+            this.btn_Unkown.Size = new System.Drawing.Size(126, 137);
+            this.btn_Unkown.TabIndex = 3;
+            this.btn_Unkown.Text = "不认识";
+            this.btn_Unkown.UseVisualStyleBackColor = true;
+            this.btn_Unkown.Click += new System.EventHandler(this.btn_Unkown_Click);
+            // 
+            // chkBox_ModeSelect
+            // 
+            this.chkBox_ModeSelect.AutoSize = true;
+            this.chkBox_ModeSelect.Checked = true;
+            this.chkBox_ModeSelect.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkBox_ModeSelect.Location = new System.Drawing.Point(3, 348);
+            this.chkBox_ModeSelect.Name = "chkBox_ModeSelect";
+            this.chkBox_ModeSelect.Size = new System.Drawing.Size(119, 19);
+            this.chkBox_ModeSelect.TabIndex = 4;
+            this.chkBox_ModeSelect.Text = "开启测验模式";
+            this.chkBox_ModeSelect.UseVisualStyleBackColor = true;
+            this.chkBox_ModeSelect.CheckedChanged += new System.EventHandler(this.chkBox_ModeSelect_CheckedChanged);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pBox_ShowPhotos, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 28);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(689, 560);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.chkBox_ModeSelect, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.lab_ShowInfo, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(416, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(270, 524);
+            this.tableLayoutPanel2.TabIndex = 6;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.btn_Unkown, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.btn_Know, 0, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 378);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(264, 143);
+            this.tableLayoutPanel3.TabIndex = 7;
+            // 
+            // pBox_ShowPhotos
+            // 
+            this.pBox_ShowPhotos.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pBox_ShowPhotos.Location = new System.Drawing.Point(3, 3);
+            this.pBox_ShowPhotos.Name = "pBox_ShowPhotos";
+            this.pBox_ShowPhotos.Size = new System.Drawing.Size(407, 524);
+            this.pBox_ShowPhotos.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pBox_ShowPhotos.TabIndex = 0;
+            this.pBox_ShowPhotos.TabStop = false;
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.工具ToolStripMenuItem,
+            this.帮助ToolStripMenuItem,
+            this.关于ToolStripMenuItem,
+            this.更新日志ToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(689, 28);
+            this.menuStrip.TabIndex = 6;
+            this.menuStrip.Text = "menuStrip";
+            // 
+            // 工具ToolStripMenuItem
+            // 
+            this.工具ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.重置熟悉度ToolStripMenuItem});
+            this.工具ToolStripMenuItem.Name = "工具ToolStripMenuItem";
+            this.工具ToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.工具ToolStripMenuItem.Text = "工具";
+            // 
+            // 重置熟悉度ToolStripMenuItem
+            // 
+            this.重置熟悉度ToolStripMenuItem.Name = "重置熟悉度ToolStripMenuItem";
+            this.重置熟悉度ToolStripMenuItem.Size = new System.Drawing.Size(159, 26);
+            this.重置熟悉度ToolStripMenuItem.Text = "重置熟悉度";
+            this.重置熟悉度ToolStripMenuItem.Click += new System.EventHandler(this.重置熟悉度ToolStripMenuItem_Click);
+            // 
+            // 帮助ToolStripMenuItem
+            // 
+            this.帮助ToolStripMenuItem.Name = "帮助ToolStripMenuItem";
+            this.帮助ToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.帮助ToolStripMenuItem.Text = "帮助";
+            this.帮助ToolStripMenuItem.Click += new System.EventHandler(this.帮助ToolStripMenuItem_Click);
+            // 
+            // 关于ToolStripMenuItem
+            // 
+            this.关于ToolStripMenuItem.Name = "关于ToolStripMenuItem";
+            this.关于ToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.关于ToolStripMenuItem.Text = "关于";
+            this.关于ToolStripMenuItem.Click += new System.EventHandler(this.关于ToolStripMenuItem_Click);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.progressBar,
+            this.Lab_ShowRem});
+            this.statusStrip.Location = new System.Drawing.Point(0, 563);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(689, 25);
+            this.statusStrip.TabIndex = 7;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(300, 19);
+            // 
+            // Lab_ShowRem
+            // 
+            this.Lab_ShowRem.Name = "Lab_ShowRem";
+            this.Lab_ShowRem.Size = new System.Drawing.Size(108, 20);
+            this.Lab_ShowRem.Text = "LabShowRem";
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1200000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // 更新日志ToolStripMenuItem
+            // 
+            this.更新日志ToolStripMenuItem.Name = "更新日志ToolStripMenuItem";
+            this.更新日志ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.更新日志ToolStripMenuItem.Text = "更新日志";
+            this.更新日志ToolStripMenuItem.Click += new System.EventHandler(this.更新日志ToolStripMenuItem_Click);
+            // 
+            // MemorizePeopleForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 588);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "MemorizePeopleForm";
+            this.Text = "照片助记助手";
+            this.Load += new System.EventHandler(this.MemorizePeopleForm_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pBox_ShowPhotos)).EndInit();
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lab_ShowInfo;
+        private System.Windows.Forms.Button btn_Know;
+        private System.Windows.Forms.Button btn_Unkown;
+        private System.Windows.Forms.CheckBox chkBox_ModeSelect;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.PictureBox pBox_ShowPhotos;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem 帮助ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 关于ToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripProgressBar progressBar;
+        private System.Windows.Forms.ToolStripStatusLabel Lab_ShowRem;
+        private System.Windows.Forms.ToolStripMenuItem 工具ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 重置熟悉度ToolStripMenuItem;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.ToolStripMenuItem 更新日志ToolStripMenuItem;
+    }
+}
+
---
+++ MemoryAssistant/MemorizePeopleForm.cs
@@ -0,0 +1,358 @@
+﻿using SharpConfig;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MemoryAssistant
+{
+    public partial class MemorizePeopleForm : Form
+    {
+        public const string VERSION = "Beta 1.1";
+        public const string VERDATE = "2016.9.3";
+
+        public float[] LevelSetting = { 1.0f, 0.8f, 0.6f, 0.4f, 0.2f };
+        public int TimeRemind = 1200000;
+
+        public bool ShowInfo
+        {
+            get
+            {
+                return m_showInfo;
+            }
+            set
+            {
+                m_showInfo = value;
+                if (m_showInfo)
+                {
+                    lab_ShowInfo.Text = Info;
+                }
+                else
+                {
+                    lab_ShowInfo.Text = "点击查看信息";
+                }
+            }
+        }
+        public Mode FormMode { get; private set; } = Mode.MEMORIZE;
+        public string Info { get; private set; }
+        public string NowID { get; private set; }
+
+        public Dictionary<string, Image> AllImg { get; private set; }
+        //public Dictionary<string, string> AllInfo { get; private set; }
+
+        private bool m_showInfo = false;
+
+        private Configuration config;
+        private Random random = new Random();
+
+        public MemorizePeopleForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MemorizePeopleForm_Load(object sender, EventArgs e)
+        {
+            //标题显示版本信息
+            Text += " - " + VERSION + " - " + VERDATE;
+
+            //更新模式
+            if (chkBox_ModeSelect.Checked)
+            {
+                FormMode = Mode.TEST;
+                ShowInfo = true;
+            }  
+            else
+            {
+                FormMode = Mode.MEMORIZE;
+                ShowInfo = false;
+            }
+
+
+            //如果没有配置文件创建
+            if(!File.Exists(Application.StartupPath + "\\config.ini"))
+            {
+                var c = new Configuration();
+                c["IniInfo"]["CreateVer"].StringValue = VERSION;
+                c["IniInfo"]["CreateDate"].StringValue = DateTime.Now.ToString();
+                c["General"]["LevelSetting"].FloatValueArray = LevelSetting;
+                c["General"]["TimeRemind"].IntValue = TimeRemind;
+                c.SaveToFile("config.ini");
+                ShowHelp();
+                ShowAbout();
+            }
+
+            //如果没有photos目录创建并报错
+            if (!Directory.Exists(Application.StartupPath + "//photos//"))
+            {
+                Directory.CreateDirectory(Application.StartupPath + "//photos//");
+                MessageBox.Show("请将照片放入photos文件夹中，并以希望显示的名字命名！");
+                this.Close();
+                return;
+            }
+
+            config = Configuration.LoadFromFile("config.ini");
+
+            //读取阶梯熟悉度设置
+            LevelSetting = config["General"]["LevelSetting"].FloatValueArray;
+
+            int timeRemind = config["General"]["TimeRemind"].IntValue;
+            if (timeRemind <= 0) timer.Enabled = false;
+            else
+            {
+                timer.Enabled = true;
+                TimeRemind = timeRemind;
+            }
+            
+            
+
+            //读取照片
+            try
+            {
+                LoadImages();
+            }
+            catch
+            {
+                this.Close();
+                return;
+            }
+
+            //自动读取模式设置
+            try
+            {
+                chkBox_ModeSelect.Checked = 
+                    config["General"]["IsTest"].BoolValue;
+            }
+            catch
+            {
+                config["General"]["IsTest"].BoolValue = chkBox_ModeSelect.Checked;
+                config.SaveToFile("config.ini");
+            }
+
+
+            Next();
+
+            UpdateStatusStrip();
+        }
+
+        private void lab_ShowInfo_Click(object sender, EventArgs e)
+        {
+            if(FormMode == Mode.TEST)
+               ShowInfo = !ShowInfo;
+        }
+
+        private void btn_Know_Click(object sender, EventArgs e)
+        {
+            if(FormMode == Mode.MEMORIZE)
+            {
+                Next();
+                return;
+            }
+            int l = LoadLevel(NowID);
+            if (l < LevelSetting.Count<float>() - 1)
+                SaveLevel(NowID, ++l);
+            else SaveLevel(NowID, LevelSetting.Count<float>() - 1);
+            Next();
+        }
+
+        private void btn_Unkown_Click(object sender, EventArgs e)
+        {
+            if (FormMode == Mode.MEMORIZE)
+            {
+                Next();
+                return;
+            }
+            int l = LoadLevel(NowID);
+            if (l > 0)
+                SaveLevel(NowID, --l);
+            else SaveLevel(NowID, 0);
+            Next();
+        }
+
+        private void LoadImages()
+        {
+            string path = Application.StartupPath + "\\photos\\";
+            DirectoryInfo theFolder = new DirectoryInfo(path);
+
+            AllImg = new Dictionary<string, Image>();
+            //AllInfo = new Dictionary<string, string>();
+
+            bool havePhotos = false;
+            //读取照片
+            foreach (FileInfo nextFile in theFolder.GetFiles())
+            {
+                if(nextFile.Extension != ".jpg" &&
+                    nextFile.Extension != ".png" &&
+                    nextFile.Extension != ".gif" &&
+                    nextFile.Extension != ".bmp" &&
+                    nextFile.Extension != ".ico" &&
+                    nextFile.Extension != ".dib" &&
+                    nextFile.Extension != ".tif")
+                         continue;
+
+                havePhotos = true;
+
+                //获取照片名作为id编号
+                string id = nextFile.Name.Split('.')[0];
+                
+
+                //AllInfo.Add(id, nextFile.Name);
+                AllImg.Add(id, Image.FromFile(nextFile.FullName));
+
+                //如果没有，创建熟练度信息
+                if (LoadLevel(id) < 0)
+                {
+                    SaveLevel(id, 0);
+                }
+                
+            }
+
+            if (!havePhotos)
+            {
+                MessageBox.Show("没有在photos文件夹中检测到照片！");
+                this.Close();
+                throw new Exception("NoPhotoInFolder");
+            }
+
+        }
+
+        public void Next()
+        {
+            string next = RandomID();
+            Info = next;
+            pBox_ShowPhotos.Image = AllImg[next];
+            NowID = next;
+            if (FormMode == Mode.MEMORIZE)
+            {
+                ShowInfo = true;
+            }
+            else
+            {
+                ShowInfo = false;
+            }
+            UpdateStatusStrip();
+        }
+
+        public string RandomID()
+        {
+            int total = AllImg.Count;
+            int ran = random.Next(total);
+            string id = AllImg.Keys.ToList<string>()[ran];
+            if(random.NextDouble() > LevelSetting[LoadLevel(id)])
+            {
+                return RandomID();
+            }
+            return id;
+        }
+        public void SaveLevel(string id, int level)
+        {
+            config["Data"][id].SetValue<int>(level);
+            config.SaveToFile("config.ini");
+        }
+        /// <summary>
+        /// 没有返回-1
+        /// </summary>
+        public int LoadLevel(string id)
+        {
+            Section section = config["Data"];
+            try
+            {
+                int result = section[id].IntValue;
+                return result;
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+
+        public dynamic Check()
+        {
+            int total = 0;
+            int remNum = 0;
+            foreach (string key in AllImg.Keys)
+            {
+                total++;
+                if (LoadLevel(key) >= LevelSetting.Count<float>() - 1)
+                    remNum++;
+            }
+            return new { TotalNumber = total, HaveBeenRemembered = remNum, Complete = remNum / (float) total * 100f };
+        }
+
+        public void UpdateStatusStrip()
+        {
+            dynamic r = Check();
+            Lab_ShowRem.Text = String.Format("您已记住{0}位，共{1}位。完成率：{2}%", r.HaveBeenRemembered, r.TotalNumber, Math.Round(r.Complete, 2));
+            progressBar.Value = (int)r.Complete;
+        }
+
+
+        public enum Mode
+        {
+            TEST, MEMORIZE
+        }
+
+        private void chkBox_ModeSelect_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkBox_ModeSelect.Checked)
+            {
+                FormMode = Mode.TEST;
+                ShowInfo = false;
+            }
+            else
+            {
+                FormMode = Mode.MEMORIZE;
+                ShowInfo = true;
+            }
+            config["General"]["IsTest"].BoolValue = chkBox_ModeSelect.Checked;
+            config.SaveToFile("config.ini");
+        }
+
+        private void 帮助ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ShowHelp();
+        }
+        public void ShowHelp()
+        {
+            MessageBox.Show("将照片放入photos文件夹中即可自动读取，并以文件名作为名字读取。\n现支持照片格式：jpg gif bmp ico png dib tif\n\n在【测验模式】下，系统会根据用户选择的【认识】与【不认识】来判断用户对某照片的熟悉程度，并按熟悉程度衰减其出现的概率。\n\nTips:此窗口大小可更改。", "帮助");
+        }
+
+        private void 关于ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ShowAbout();
+        }
+        public void ShowAbout()
+        {
+            MessageBox.Show(String.Format("版本号：{0}\nRelease日期：{1}\n本软件为免费软件,未经许可禁止用于商业用途！", VERSION, VERDATE), "关于");
+        }
+
+        private void 重置熟悉度ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (string key in AllImg.Keys)
+            {
+                SaveLevel(key, 0);
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            MessageBox.Show("您已经连续记忆" + TimeRemind / 1000 / 60 + "分钟了。\n休息一下吧！", "休息一下");
+        }
+
+        private void 更新日志ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            UpdateList();
+        }
+
+        public void UpdateList()
+        {
+            MessageBox.Show("Alpha 1.0\n    进行内部测试。\n\nBeta 1.0\n    小范围发布测试。\n\nBeta 1.1\n    ·增加了长时间提醒和更新日志。", "更新日志");
+        }
+    }
+}
